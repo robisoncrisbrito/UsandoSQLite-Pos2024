4160c2a6951d5ca1ede247f3dff0524087ac8595
robisoncrisbrito <robison.brito@gmail.com>
2024-08-03 14:15:08 -0300

    .

---
+++ app/src/main/java/br/edu/utfpr/usandosqlite_pos2024/adapter/MeuAdapter.kt
@@ -0,0 +1,23 @@
+package br.edu.utfpr.usandosqlite_pos2024.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class MeuAdapter : BaseAdapter() {
+    override fun getCount(): Int {
+
+    }
+
+    override fun getItem(position: Int): Any {
+
+    }
+
+    override fun getItemId(position: Int): Long {
+
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+    }
+}
