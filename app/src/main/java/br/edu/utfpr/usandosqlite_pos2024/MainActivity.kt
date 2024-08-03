581794e7de9a570bd2aa57c7b241b8f4ce28eb1d
robisoncrisbrito <robison.brito@gmail.com>
2024-08-03 10:19:53 -0300

    .

--- app/src/main/java/br/edu/utfpr/usandosqlite_pos2024/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/usandosqlite_pos2024/MainActivity.kt
@@ -1,15 +1,18 @@
 package br.edu.utfpr.usandosqlite_pos2024
 
+import android.content.ContentValues
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.utfpr.usandosqlite_pos2024.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
     private lateinit var banco : SQLiteDatabase
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,25 +53,83 @@ class MainActivity : AppCompatActivity() {
 
     private fun btIncluirOnClick() {
 
+        val registro = ContentValues()
+        registro.put( "nome", binding.etNome.text.toString() )
+        registro.put( "telefone", binding.etTelefone.text.toString() )
+
+        banco.insert( "cadastro", null, registro )
+
+        Toast.makeText( this, "Sucesso!!!", Toast.LENGTH_LONG ).show()
+
     }
 
     private fun btAlterarOnClick() {
+        val registro = ContentValues()
+        registro.put( "nome", binding.etNome.text.toString() )
+        registro.put( "telefone", binding.etTelefone.text.toString() )
 
+        banco.update( "cadastro", registro, "_id=${binding.etCod.text.toString()}", null )
+
+        Toast.makeText( this, "Sucesso!!!", Toast.LENGTH_LONG ).show()
     }
 
     private fun btExcluirOnClick() {
-
+        banco.delete( "cadastro", "_id=${binding.etCod.text.toString()}", null)
+        Toast.makeText( this, "Sucesso!!!", Toast.LENGTH_LONG ).show()
     }
 
     private fun btPesquisarOnClick() {
+        val registro =  banco.query(
+            "cadastro",
+            null,
+            "_id=${binding.etCod.text.toString()}",
+            null,
+            null,
+            null,
+            null
+        )
+
+        if ( registro.moveToNext() ) {
+            binding.etNome.setText( registro.getString(Companion.NOME) )
+            binding.etTelefone.setText( registro.getString(Companion.TELEFONE) )
+        } else {
+            Toast.makeText( this, "Registro não encontrado", Toast.LENGTH_LONG ).show()
+        }
+
+
 
     }
 
     private fun btPListarOnClick() {
+        val registro =  banco.query(
+            "cadastro",
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        )
 
+        var saida = StringBuilder()
+
+        while ( registro.moveToNext() ) {
+            saida.append( registro.getInt(Companion.COD) )
+            saida.append( "-" )
+            saida.append( registro.getString(Companion.NOME) )
+            saida.append( "-" )
+            saida.append( registro.getString(Companion.TELEFONE) )
+            saida.append( "\n" )
+        }
+
+        Toast.makeText( this, saida.toString(), Toast.LENGTH_LONG ).show()
     }
 
-
+    companion object {
+        private const val COD = 0
+        private const val NOME = 1
+        private const val TELEFONE = 2
+    }
 
 
 }
