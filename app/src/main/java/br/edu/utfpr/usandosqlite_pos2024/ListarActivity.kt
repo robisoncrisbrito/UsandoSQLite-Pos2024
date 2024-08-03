d735f561dab93f4f890b653d8f6eea7baa8858d7
robisoncrisbrito <robison.brito@gmail.com>
2024-08-03 13:32:48 -0300

    .

--- app/src/main/java/br/edu/utfpr/usandosqlite_pos2024/ListarActivity.kt
+++ app/src/main/java/br/edu/utfpr/usandosqlite_pos2024/ListarActivity.kt
@@ -1,14 +1,20 @@
 package br.edu.utfpr.usandosqlite_pos2024
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.edu.utfpr.usandosqlite_pos2024.databinding.ActivityListarBinding
 
 class ListarActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityListarBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_listar)
+        binding = ActivityListarBinding.inflate( layoutInflater )
+        setContentView(binding.root)
+
+        val registros = listOf<String>( "Brasil", "Argentina", "Paraguai", "Uruguai" ) //fonte
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, registros )//meio de campo
+        binding.lvPrincipal.adapter = adapter
     }
 }
