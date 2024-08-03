4107f9696ca46109321a24718b65321f1cbed7b0
robisoncrisbrito <robison.brito@gmail.com>
2024-08-03 11:26:06 -0300

    .

---
+++ app/src/main/java/br/edu/utfpr/usandosqlite_pos2024/database/DatabaseHandler.kt
@@ -0,0 +1,63 @@
+package br.edu.utfpr.usandosqlite_pos2024.database
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import br.edu.utfpr.usandosqlite_pos2024.entity.Cadastro
+
+class DatabaseHandler (context : Context ) : SQLiteOpenHelper ( context, DATABASE_NAME, null, DATABASE_VERSION ) {
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL( "CREATE TABLE IF NOT EXISTS ${TABLE_NAME} (_id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, telefone TEXT ) ")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL( "DROP TABLE IF EXISTS ${TABLE_NAME}" )
+        onCreate( db )
+    }
+
+    companion object {
+        private const val DATABASE_NAME = "dbfile.sqlite"
+        private const val DATABASE_VERSION = 1
+        private const val TABLE_NAME = "cadastro"
+        private const val COD = 0
+        private const val NOME = 1
+        private const val TELEFONE = 2
+    }
+
+    fun insert( cadastro : Cadastro) {
+        val db = this.writableDatabase
+
+        val registro = ContentValues()
+        registro.put( "nome", cadastro.nome )
+        registro.put( "telefone", cadastro.telefone )
+
+        db.insert( TABLE_NAME, null, registro )
+    }
+
+    fun update(cadastro : Cadastro) {
+        val db = this.writableDatabase
+
+        val registro = ContentValues()
+        registro.put( "nome", cadastro.nome )
+        registro.put( "telefone", cadastro.telefone )
+
+        db.update( TABLE_NAME, registro, "_id=${cadastro._id}", null )
+    }
+
+    fun delete( id : Int ) {
+        val db = this.writableDatabase
+
+        db.delete( TABLE_NAME, "_id=${id}", null)
+    }
+
+    fun find() {
+
+    }
+
+    fun list() {
+
+    }
+
+}
---
+++ app/src/main/java/br/edu/utfpr/usandosqlite_pos2024/entity/Cadastro.kt
@@ -0,0 +1,3 @@
+package br.edu.utfpr.usandosqlite_pos2024.entity
+
+class Cadastro (var _id: Int, var nome : String, var telefone : String )
