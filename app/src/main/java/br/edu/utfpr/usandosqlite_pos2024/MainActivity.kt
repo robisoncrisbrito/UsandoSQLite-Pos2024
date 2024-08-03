3bf66f12bdef34724ded75bf22966bc203d0868d
robisoncrisbrito <robison.brito@gmail.com>
2024-08-03 09:26:36 -0300

    .

--- app/src/main/java/br/edu/utfpr/usandosqlite_pos2024/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/usandosqlite_pos2024/MainActivity.kt
@@ -1,20 +1,64 @@
 package br.edu.utfpr.usandosqlite_pos2024
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.edu.utfpr.usandosqlite_pos2024.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate( layoutInflater )
+        setContentView(binding.root )
+
+        setButtonListener()
+    }
+
+    private fun setButtonListener() {
+        binding.btIncluir.setOnClickListener{
+            btIncluirOnClick()
+        }
+
+        binding.btAlterar.setOnClickListener{
+            btAlterarOnClick()
+        }
+
+        binding.btExcluir.setOnClickListener{
+            btExcluirOnClick()
+        }
+
+        binding.btPesquisar.setOnClickListener{
+            btPesquisarOnClick()
+        }
+
+        binding.btListar.setOnClickListener{
+            btPListarOnClick()
         }
     }
+
+    private fun btIncluirOnClick() {
+
+    }
+
+    private fun btAlterarOnClick() {
+
+    }
+
+    private fun btExcluirOnClick() {
+
+    }
+
+    private fun btPesquisarOnClick() {
+
+    }
+
+    private fun btPListarOnClick() {
+
+    }
+
+
+
+
 }
